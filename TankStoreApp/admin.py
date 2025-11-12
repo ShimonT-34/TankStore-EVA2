5e131820d90254af64100c0f9eb9fb67881f601d
Talleres Puente Alto <tallerespa@inacapmail.cl>
2025-11-12 15:56:15 -0300

    commit con el gitignore y el listdisplay que faltaba

--- TankStoreApp/admin.py
+++ TankStoreApp/admin.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from . models import Tanque, Categoria
 
 # Register your models here.
+class CategoriaAdmin(admin.ModelAdmin):
+    listdisplay = ["nombre_tanque", "precio", "stock", "descripcion", "imagen", "pais", "categoria"]
 
 admin.site.register(Tanque)
-admin.site.register(Categoria)
+admin.site.register(Categoria, CategoriaAdmin)
